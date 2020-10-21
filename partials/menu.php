679a4f8c825cc7a0298cc32e1d646da006865b0f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-10-21 17:00:21 -0300

    Ajustes em classes do menu principal

--- partials/menu.php
+++ partials/menu.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light collapse show">
+<nav class="navbar navbar-light navbar-expand-lg p-0 collapse show">
     <button class="navbar-toggler" data-toggle="collapse" data-target="#navbar-principal" aria-controls="navbar-principal" aria-expanded="false"><span class="navbar-toggler-icon" aria-hidden="true"></span><span class="sr-only">Esconder/Mostrar</span>&nbsp;Menu</button>
     <?php
         wp_nav_menu(
             array(
-                'menu_class'      => 'navbar-nav mr-auto menu-principal',
+                'menu_class'      => 'navbar-nav menu-principal',
                 'menu_id'         => false,
                 'container'       => 'div',
                 'container_class' => 'collapse navbar-collapse',
