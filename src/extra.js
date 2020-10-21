868da8663d30a86cb885c066f3e954e507f25f0d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-10-21 17:00:58 -0300

    Adição de código de exemplo do customizer

--- src/extra.js
+++ src/extra.js
@@ -3,5 +3,7 @@ require('bootstrap');
 require('lazysizes');
 require('lazysizes/plugins/native-loading/ls.native-loading');
 
+require('./modules/wp-customizer');
+
 require('./modules/picturefill-config');
 require('./modules/fancybox-config');
