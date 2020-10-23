a561d6ef0f82d7e249e63aaeea7e995cd7ec1d68
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-10-23 18:05:29 -0300

    Remoção do JS para o customizer

--- src/extra.js
+++ src/extra.js
@@ -3,8 +3,6 @@ require('bootstrap');
 require('lazysizes');
 require('lazysizes/plugins/native-loading/ls.native-loading');
 
-require('./modules/wp-customizer');
-
 require('./modules/picturefill-config');
 require('./modules/fancybox-config');
 require('./modules/tooltip');
