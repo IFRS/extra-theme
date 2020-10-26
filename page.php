918ce429663e9133890c2bb902ab7b39a6941be1
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-10-26 12:33:10 -0300

    Criação do template para páginas

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="row">
+    <div class="col">
+        <article>
+            <h2><?php the_title(); ?></h2>
+            <?php the_content(); ?>
+        </article>
+    </div>
+</div>
+
+<?php get_footer(); ?>
