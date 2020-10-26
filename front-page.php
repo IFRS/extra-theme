a1b475bdf771cb113d6261475edb222b538bd61a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-10-26 12:32:38 -0300

    Ajuste na página inicial

--- front-page.php
+++ front-page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-<?php the_post(); ?>
-
 <?php if (get_option('show_on_front') === 'page') : ?>
+    <?php the_post(); ?>
+
     <h2><?php the_title(); ?></h2>
+
     <?php the_content(); ?>
 <?php endif; ?>
 
