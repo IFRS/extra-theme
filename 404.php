9d28259dbaaa981697e73da2fa39badd5d974014
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-10-28 11:41:59 -0300

    Criação do template para página não encontrada

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="row">
+    <div class="col-12">
+        <div class="alert alert-danger text-center" role="alert">
+            <strong>Ops!&nbsp;</strong>
+            <br>
+            A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada.
+            <br>
+            Tente fazer uma busca pelo site...
+            <?php get_search_form(); ?>
+            ou, voc&ecirc; pode voltar para a <a href="<?php echo esc_url(home_url()); ?>" class="alert-link">p&aacute;gina inicial</a>.
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
