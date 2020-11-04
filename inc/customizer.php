7bb0dd98f618cffa4e76592ac0295b8a0eb12235
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-11-04 14:39:03 -0300

    Adição de sanitização para Unidade no customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,8 +1,18 @@
 <?php
     add_action('customize_register', function($wp_customize) {
         $wp_customize->add_setting( 'ifrs_unidade' , array(
-            'default'   => 'reitoria',
-            'transport' => 'refresh',
+            'default'           => 'reitoria',
+            'transport'         => 'refresh',
+            'sanitize_callback' => function($input, $setting) {
+                // Ensure input is a slug.
+                $input = sanitize_key( $input );
+
+                // Get list of choices from the control associated with the setting.
+                $choices = $setting->manager->get_control( $setting->id )->choices;
+
+                // If the input is a valid key, return it; otherwise, return the default.
+                return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
+            }
         ) );
         $wp_customize->add_control(
             'ifrs_unidade_control',
@@ -24,109 +34,109 @@
                 'option' => 'Geral',
                 'title'  => 'Instituto Federal do Rio Grande do Sul',
                 'url'    => 'https://ifrs.edu.br/',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/ifrs.png',
+                'image'  => get_template_directory_uri().'/img/marcas/ifrs.png',
             ),
             'alvorada' => array(
                 'option' => 'Alvorada',
                 'title'  => 'Campus Alvorada',
                 'url'    => 'https://ifrs.edu.br/alvorada',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/alvorada.png',
+                'image'  => get_template_directory_uri().'/img/marcas/alvorada.png',
             ),
             'bento' => array(
                 'option' => 'Bento Gonçalves',
                 'title'  => 'Campus Bento Gonçalves',
                 'url'    => 'https://ifrs.edu.br/bento',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/bento.png',
+                'image'  => get_template_directory_uri().'/img/marcas/bento.png',
             ),
             'canoas' => array(
                 'option' => 'Canoas',
                 'title'  => 'Campus Canoas',
                 'url'    => 'https://ifrs.edu.br/canoas',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/canoas.png',
+                'image'  => get_template_directory_uri().'/img/marcas/canoas.png',
             ),
             'caxias' => array(
                 'option' => 'Caxias do Sul',
                 'title'  => 'Campus Caxias do Sul',
                 'url'    => 'https://ifrs.edu.br/caxias',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/caxias.png',
+                'image'  => get_template_directory_uri().'/img/marcas/caxias.png',
             ),
             'erechim' => array(
                 'option' => 'Erechim',
                 'title'  => 'Campus Erechim',
                 'url'    => 'https://ifrs.edu.br/erechim',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/erechim.png',
+                'image'  => get_template_directory_uri().'/img/marcas/erechim.png',
             ),
             'farroupilha' => array(
                 'option' => 'Farroupilha',
                 'title'  => 'Campus Farroupilha',
                 'url'    => 'https://ifrs.edu.br/farroupilha',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/farroupilha.png',
+                'image'  => get_template_directory_uri().'/img/marcas/farroupilha.png',
             ),
             'feliz' => array(
                 'option' => 'Feliz',
                 'title'  => 'Campus Feliz',
                 'url'    => 'https://ifrs.edu.br/feliz',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/feliz.png',
+                'image'  => get_template_directory_uri().'/img/marcas/feliz.png',
             ),
             'ibiruba' => array(
                 'option' => 'Ibirubá',
                 'title'  => 'Campus Ibirubá',
                 'url'    => 'https://ifrs.edu.br/ibiruba',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/ibiruba.png',
+                'image'  => get_template_directory_uri().'/img/marcas/ibiruba.png',
             ),
             'osorio' => array(
                 'option' => 'Osório',
                 'title'  => 'Campus Osório',
                 'url'    => 'https://ifrs.edu.br/osorio',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/osorio.png',
+                'image'  => get_template_directory_uri().'/img/marcas/osorio.png',
             ),
             'poa' => array(
                 'option' => 'Porto Alegre',
                 'title'  => 'Campus Porto Alegre',
                 'url'    => 'https://poa.ifrs.edu.br/',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/poa.png',
+                'image'  => get_template_directory_uri().'/img/marcas/poa.png',
             ),
             'restinga' => array(
                 'option' => 'Restinga',
                 'title'  => 'Campus Restinga',
                 'url'    => 'https://ifrs.edu.br/restinga',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/restinga.png',
+                'image'  => get_template_directory_uri().'/img/marcas/restinga.png',
             ),
             'riogrande' => array(
                 'option' => 'Rio Grande',
                 'title'  => 'Campus Rio Grande',
                 'url'    => 'https://ifrs.edu.br/riogrande',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/riogrande.png',
+                'image'  => get_template_directory_uri().'/img/marcas/riogrande.png',
             ),
             'rolante' => array(
                 'option' => 'Rolante',
                 'title'  => 'Campus Rolante',
                 'url'    => 'https://ifrs.edu.br/rolante',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/rolante.png',
+                'image'  => get_template_directory_uri().'/img/marcas/rolante.png',
             ),
             'sertao' => array(
                 'option' => 'Sertão',
                 'title'  => 'Campus Sertão',
                 'url'    => 'https://ifrs.edu.br/sertao',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/sertao.png',
+                'image'  => get_template_directory_uri().'/img/marcas/sertao.png',
             ),
             'vacaria' => array(
                 'option' => 'Vacaria',
                 'title'  => 'Campus Vacaria',
                 'url'    => 'https://ifrs.edu.br/vacaria',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/vacaria.png',
+                'image'  => get_template_directory_uri().'/img/marcas/vacaria.png',
             ),
             'veranopolis' => array(
                 'option' => 'Veranópolis',
                 'title'  => 'Campus Veranópolis',
                 'url'    => 'https://ifrs.edu.br/veranopolis',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/veranopolis.png',
+                'image'  => get_template_directory_uri().'/img/marcas/veranopolis.png',
             ),
             'viamao' => array(
                 'option' => 'Viamão',
                 'title'  => 'Campus Viamão',
                 'url'    => 'https://ifrs.edu.br/viamao',
-                'image'  => get_stylesheet_directory_uri().'/img/marcas/viamao.png',
+                'image'  => get_template_directory_uri().'/img/marcas/viamao.png',
             ),
         );
     }
