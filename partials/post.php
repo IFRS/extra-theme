0cc1c3271d83af33695f68f112654c890e6d375d
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-11-09 17:08:42 -0300

    Ajuste na estilização do meta do post item

--- partials/post.php
+++ partials/post.php
@@ -1,17 +1,17 @@
 <article class="post-item">
     <h3 class="post-item__titulo"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-    <time class="post-item__data" datetime="<?php echo get_the_date('c'); ?>"><?php the_time('d.m.Y'); ?></time>
-    &centerdot;
-    <?php the_category(', '); ?>
+    <div class="post-item__meta">
+        <time class="post-item__data" datetime="<?php echo get_the_date('c'); ?>"><?php the_time('d.m.Y'); ?></time>
+        &centerdot;
+        <?php the_category(', '); ?>
+    </div>
     <div class="post-item__resumo">
         <?php the_excerpt(); ?>
     </div>
     <?php if (comments_open()) : ?>
         <hr class="post-item__separador">
-        <div class="post-item__meta">
-            <span class="post-item__comentarios">
-                <?php echo get_comments_number(); ?>
-            </span>
-        </div>
+        <span class="post-item__comentarios">
+            <?php echo get_comments_number(); ?>
+        </span>
     <?php endif; ?>
 </article>
